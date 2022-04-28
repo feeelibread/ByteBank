5eeb5ad8c1c802e1cbbed651f7e1cecdb545d391
Felipe Pichinine <feeelpoos@gmail.com>
2022-04-28 12:29:00 -0300

    Atribuindo os dados da conta corrente para os atributos, e criando a classe ContaCorrente

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bem vindo a seu banco ByteBank!");
 
+string nome = "Felipe Piquinique";
+string conta = "10123-X";
+int numeroAgencia = 42;
+string nomeAgencia = "Alliance Bank";
+
+double saldo = 200.50;
+Console.WriteLine($"Titular: {nome}");
+Console.WriteLine($"Conta: {conta}");
+Console.WriteLine($"Nome da agência: {nomeAgencia}");
+Console.WriteLine($"Numero da agência: {numeroAgencia}");
+Console.WriteLine($"Saldo da conta: {saldo.ToString("C2")}");
+Console.ReadKey();

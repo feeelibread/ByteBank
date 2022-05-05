74b9c017beabc1ae3acb58949204c1bedb8ff1bd
Felipe Pichinine <feeelpoos@gmail.com>
2022-05-05 11:37:17 -0300

    Instanciando um objeto

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,14 +1,20 @@
-﻿Console.WriteLine("Bem vindo a seu banco ByteBank!");
+﻿using ByteBank;
 
-string nome = "Felipe Piquinique";
-string conta = "10123-X";
-int numeroAgencia = 42;
-string nomeAgencia = "Alliance Bank";
+Console.WriteLine("Bem vindo a seu banco ByteBank!");
 
-double saldo = 200.50;
-Console.WriteLine($"Titular: {nome}");
-Console.WriteLine($"Conta: {conta}");
-Console.WriteLine($"Nome da agência: {nomeAgencia}");
-Console.WriteLine($"Numero da agência: {numeroAgencia}");
-Console.WriteLine($"Saldo da conta: {saldo.ToString("C2")}");
+ContaCorrente conta1 = new ContaCorrente();
+
+conta1.titular = "Felipe";
+conta1.conta = "15926-X";
+conta1.nomeAgencia = "Strattovarius";
+conta1.numeroAgencia = 64;
+conta1.saldo = 159.52;
+
+
+
+Console.WriteLine($"Titular: {conta1.titular}");
+Console.WriteLine($"Conta: {conta1.conta}");
+Console.WriteLine($"Nome da agência: {conta1.nomeAgencia}");
+Console.WriteLine($"Numero da agência: {conta1.numeroAgencia}");
+Console.WriteLine($"Saldo da conta: {conta1.saldo:C2}");
 Console.ReadKey();

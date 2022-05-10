ce66a9560ce5c0c0cd8d27babe0dccbe58d32816
Felipe Pichinine <feeelpoos@gmail.com>
2022-05-10 15:39:28 -0300

    Criação dos métodos Sacar, Depositar e Transferir onde o Método Depositar é do tipo Void e não retorna valor

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -7,6 +7,44 @@
         public int numeroAgencia;
         public string nomeAgencia;
         public double saldo;
-        public bool validacao;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                Console.WriteLine($"O saque não foi efetuado pois o valor de {valor:C2} é maior que o seu saldo atual de {saldo:C2}");
+                return false;
+            }
+
+            if (valor < 0)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente conta)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            if (valor < 0)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            conta.saldo += valor;
+            return true;
+        }
     }
 }

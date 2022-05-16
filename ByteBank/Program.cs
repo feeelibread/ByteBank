705d12d1a5db9798564ee3dc2865a96d0451415f
Felipe Pichinine <feeelpoos@gmail.com>
2022-05-16 12:12:52 -0300

    Criação da classe titular e alteração do Namespace

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,23 +1,12 @@
-﻿using ByteBank;
+﻿using ByteBank.Titular;
 
 Console.WriteLine("Bem vindo a seu banco ByteBank!");
 
-ContaCorrente conta1 = new ContaCorrente();
-conta1.titular = "Vinicius Cristo vão";
-conta1.conta = "68464-X";
-conta1.nomeAgencia = "Gilmar Bancus";
-conta1.numeroAgencia = 69;
-conta1.saldo = 6900.00;
+Titular titular = new Titular();
+titular.nome = "Felipe Ferreira";
+titular.cpf = "12324884669";
+titular.profissao = "Desenvolvedor C# Junior";
 
-ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "Felipus Pichininus";
-conta2.conta = "68464-X";
-conta2.nomeAgencia = "Hankus Bancus";
-conta2.numeroAgencia = 89;
-conta2.saldo = 2800.00;
-
-Console.WriteLine($"Conta pré-transferencia: conta1 saldo {conta1.saldo:C2}, e conta2 saldo {conta2.saldo}");
-conta1.Transferir(200, conta2);
-Console.WriteLine($"Conta pós-transferencia: conta1 saldo {conta1.saldo:C2}, e conta2 saldo {conta2.saldo}");
+Console.WriteLine(titular.nome);
 
 Console.ReadKey();
